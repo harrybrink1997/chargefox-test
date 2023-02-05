a30d123a2c0cbae9d730218270c7d79ff1d957af
harrybrink1997 <48706732+harrybrink1997@users.noreply.github.com>
2023-02-05 14:48:13 +1100

    changed the charger_app file to have a ChargingApp class [Binstead of just a main function. This will allow functionality with the testing after learning what rspec is.

--- lib/charger_app.rb
+++ lib/charger_app.rb
@@ -9,63 +9,60 @@ require_relative 'Vehicle.rb'
 require_relative 'MeterValue.rb'
 require_relative 'User.rb'
 
+class ChargingApp
 
-#Inclusions
+  # Main fuction for coding test.
+  # Inputs: None.
+  # Outputs: None.
+  def call(meter_json, sessions_json, vehicles_json)
 
-# Main fuction for coding test.
-# Inputs: None.
-# Outputs: None.
-def main
+    # Parse the json data files into the application.
+    # sessions_file = File.open("../data/charge_sessions.json", "r")
+    # meter_file = File.open("../data/meter_values.json", "r")
+    # vehicles_file = File.open("../data/vehicles.json", "r")
 
-  # Parse the json data files into the application.
-  sessions_file = File.open("../data/charge_sessions.json", "r")
-  meter_file = File.open("../data/meter_values.json", "r")
-  vehicles_file = File.open("../data/vehicles.json", "r")
+    # sessions_json = JSON.parse(sessions_file.read)
+    # meter_json = JSON.parse(meter_file.read)
+    # vehicles_json = JSON.parse(vehicles_file.read)
 
-  sessions_json = JSON.parse(sessions_file.read)
-  meter_json = JSON.parse(meter_file.read)
-  vehicles_json = JSON.parse(vehicles_file.read)
+    users = {}
 
-  users = {}
-
-  # Process and aggregate vehicle and user data.
-  vehicles_json.each do |vehicle|
-    if (vehicle["user"] != '')
-      new_user = User.new(vehicle["user"])
-      new_user.addVehicle(Vehicle.new(vehicle["make"], vehicle["model"]))
-      users[vehicle["user"]] = new_user
+    # Process and aggregate vehicle and user data.
+    vehicles_json.each do |vehicle|
+      if (vehicle["user"] != '')
+        new_user = User.new(vehicle["user"])
+        new_user.addVehicle(Vehicle.new(vehicle["make"], vehicle["model"]))
+        users[vehicle["user"]] = new_user
+      end
     end
+
+    sessions = {}
+
+    # Process both the session data
+    sessions_json.each do |session|
+      sessions[session["id"]] = ChargingSession.new(session['id'], session['user'])
+    end
+
+    # Process both the meter data
+    meter_json.each do |meter|
+      sessions[meter["charge_session_id"]].addMeterValue(MeterValue.new(meter["charge_session_id"], meter["amount_of_charge"], meter["rate_of_charge"], meter["timestamp"]))
+    end
+    
+    # Aggregate the session data into the user data.
+    sessions.values.each do |session|
+      users[session.user].addSession(session)
+    end
+
+    users = users.values
+    session_data_output = []
+
+    # Pull all session data from each user in json format.
+    users.each do |user|
+      session_data_output.append(user.generateSessionJsons())
+    end
+
+    puts(session_data_output)
+
+    return JSON.generate(session_data_output)
+
   end
-
-  sessions = {}
-
-  # Process both the session data
-  sessions_json.each do |session|
-    sessions[session["id"]] = ChargingSession.new(session['id'], session['user'])
-  end
-
-  # Process both the meter data
-  meter_json.each do |meter|
-    sessions[meter["charge_session_id"]].addMeterValue(MeterValue.new(meter["charge_session_id"], meter["amount_of_charge"], meter["rate_of_charge"], meter["timestamp"]))
-  end
-  
-  # Aggregate the session data into the user data.
-  sessions.values.each do |session|
-    users[session.user].addSession(session)
-  end
-
-  users = users.values
-  session_data_output = []
-
-  # Pull all session data from each user in json format.
-  users.each do |user|
-    session_data_output.append(user.generateSessionJsons())
-  end
-
-  puts(session_data_output)
-
-  return JSON.generate(session_data_output)
-
-end
-
-main()
